b8f006b35896737e5676992b3b8a915618b03a10
Daryl Jewkes <github@daryljewkes.com>
2017-02-28 20:24:44 +0000

    building basic app framework

--- skill/src/index.js
+++ skill/src/index.js
@@ -1,26 +1,35 @@
 "use strict"
 
+var async = require('async');
 var questions = require('./questions.js');
 var console = require('tracer').colorConsole();
 
-var uri = process.argv[2];
+var uri = process.env.quizURI || process.argv[2];
 
-questions.getCategories(uri + 'api_category.php', function (err, result) {
-  console.log(err, result);
+async.parallel({
+  getEasy: function (cb) {
+    questions.getQuestions(uri + 'api.php?amount=1&difficulty=easy', function (err, result) {
+      return cb(err, result);
+    });
+  },
+  getMedium: function (cb) {
+    questions.getQuestions(uri + 'api.php?amount=1&difficulty=medium', function (err, result) {
+      return cb(err, result);
+    });
+  },
+  getHard: function (cb) {
+    questions.getQuestions(uri + 'api.php?amount=1&difficulty=hard', function (err, result) {
+    return cb(err, result);
+    });
+  }
+}, function(err, results) {
+  console.log(err, JSON.stringify(results));
 });
 
-questions.getQuestions(uri + 'api.php?amount=1&difficulty=easy', function (err, result) {
-  console.log(err, result);
-});
-
-questions.getQuestions(uri + 'api.php?amount=1&difficulty=medium', function (err, result) {
-  console.log(err, result);
-});
-
-questions.getQuestions(uri + 'api.php?amount=1&difficulty=hard', function (err, result) {
-  console.log(err, result);
-});
-
-questions.getSessionKey(uri + 'api_token.php?command=request', function (err, result) {
-  console.log(err, result);
-});
+// questions.getCategories(uri + 'api_category.php', function (err, result) {
+//   console.log(err, result);
+// });
+//
+// questions.getSessionKey(uri + 'api_token.php?command=request', function (err, result) {
+//   console.log(err, result);
+// });
