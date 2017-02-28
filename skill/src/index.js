acfca9a0bef5a278a23425eb8f7e08211eaf289d
Daryl Jewkes <github@daryljewkes.com>
2017-02-28 19:44:50 +0000

    building basic app framework

--- skill/src/index.js
+++ skill/src/index.js
@@ -1,1 +1,26 @@
 "use strict"
+
+var questions = require('./questions.js');
+var console = require('tracer').colorConsole();
+
+var uri = process.argv[2];
+
+questions.getCategories(uri + 'api_category.php', function (err, result) {
+  console.log(err, result);
+});
+
+questions.getQuestions(uri + 'api.php?amount=1&difficulty=easy', function (err, result) {
+  console.log(err, result);
+});
+
+questions.getQuestions(uri + 'api.php?amount=1&difficulty=medium', function (err, result) {
+  console.log(err, result);
+});
+
+questions.getQuestions(uri + 'api.php?amount=1&difficulty=hard', function (err, result) {
+  console.log(err, result);
+});
+
+questions.getSessionKey(uri + 'api_token.php?command=request', function (err, result) {
+  console.log(err, result);
+});
---
+++ skill/src/questions.js
@@ -0,0 +1,43 @@
+"use strict"
+
+var request = require('request');
+var console = require('tracer').colorConsole();
+
+module.exports = {
+  getCategories: function (uri, callback) {
+    return getCategories(uri, callback);
+  },
+  getQuestions: function (uri, callback) {
+    return getQuestions(uri, callback);
+  },
+  getSessionKey: function (uri, callback) {
+    return getSessionKey(uri, callback);
+  }
+}
+
+function getCategories(uri, callback) {
+  request.get({
+    headers: {'content-type':'application/json'},
+    url:     uri
+  }, function(err, response, body){
+    return callback(err, JSON.parse(body))
+  });
+}
+
+function getQuestions(uri, callback) {
+  request.get({
+    headers: {'content-type':'application/json'},
+    url:     uri
+  }, function(err, response, body){
+    return callback(err, JSON.parse(body))
+  });
+}
+
+function getSessionKey(uri, callback) {
+  request.get({
+    headers: {'content-type':'application/json'},
+    url:     uri
+  }, function(err, response, body){
+    return callback(err, JSON.parse(body))
+  });
+}
